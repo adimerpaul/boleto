18315229814626d4757f8f75ee31da81da0c0271
adimerpaul <adimer101@gmail.com>
2025-04-25 05:45:06 -0400

    Actulizado footer

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import Footer from "./components/Footer.vue";
 </script>
 
 <template>
@@ -737,85 +738,7 @@ import HelloWorld from './components/HelloWorld.vue'
     </div>
   </section>
   <!-- ==========Movie-Section========== -->
-  <!-- ==========Newslater-Section========== -->
-  <footer class="footer-section">
-    <div class="newslater-section padding-bottom">
-      <div class="container">
-        <div class="newslater-container bg_img" data-background="/assets/images/newslater/newslater-bg01.jpg">
-          <div class="newslater-wrapper">
-            <h5 class="cate">subscribe to Boleto </h5>
-            <h3 class="title">to get exclusive benifits</h3>
-            <form class="newslater-form">
-              <input type="text" placeholder="Your Email Address">
-              <button type="submit">subscribe</button>
-            </form>
-            <p>We respect your privacy, so we never share your info</p>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="container">
-      <div class="footer-top">
-        <div class="logo">
-          <a href="index-1.html">
-            <img src="/assets/images/footer/footer-logo.png" alt="footer">
-          </a>
-        </div>
-        <ul class="social-icons">
-          <li>
-            <a href="#0">
-              <i class="fab fa-facebook-f"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#0" class="active">
-              <i class="fab fa-twitter"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#0">
-              <i class="fab fa-pinterest-p"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#0">
-              <i class="fab fa-google"></i>
-            </a>
-          </li>
-          <li>
-            <a href="#0">
-              <i class="fab fa-instagram"></i>
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div class="footer-bottom">
-        <div class="footer-bottom-area">
-          <div class="left">
-            <p>Copyright © 2020.All Rights Reserved By <a href="#0">Boleto </a></p>
-          </div>
-          <ul class="links">
-            <li>
-              <a href="#0">About</a>
-            </li>
-            <li>
-              <a href="#0">Terms Of Use</a>
-            </li>
-            <li>
-              <a href="#0">Privacy Policy</a>
-            </li>
-            <li>
-              <a href="#0">FAQ</a>
-            </li>
-            <li>
-              <a href="#0">Feedback</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </footer>
-  <!-- ==========Newslater-Section========== -->
+  <Footer />
 </template>
 
 <!--<style scoped>-->
